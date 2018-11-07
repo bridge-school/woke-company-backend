2140ccd11deeae966fe1673b5df462f356026bf1
samrichca <samrichca@users.noreply.github.com>
2018-11-06 19:48:05 -0500

    fix cors, remove yarn.lock

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const { errorHandler } = require("./middleware/error-handler");
 
 // Create a new express application instance
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // The port the express app will listen on
 const port = process.env.PORT || 8081;
