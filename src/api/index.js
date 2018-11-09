e7e9d71f64ff53ae84feea82861b83f6de3e6c25
ellypham <elly.pham@glossier.com>
2018-11-08 19:34:01 -0500

    Create an endpoint GET /keywords

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const { healthRouter } = require("../routes/health/health.router");
 const { checklistRouter } = require("../routes/checklist/checklist.router");
+const { keywordsRouter } = require("../routes/keywords/keywords.router");
 
 const router = express.Router();
 router.use("/health", healthRouter);
 router.use("/checklist", checklistRouter);
-
+router.use("/keywords", keywordsRouter);
 module.exports = router;
