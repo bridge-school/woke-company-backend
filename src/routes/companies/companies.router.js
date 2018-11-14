8ad04343a9fd70ef2d1c3c0b6b5365cce7761f33
Sam Rich <samrichca@users.noreply.github.com>
2018-11-13 20:51:36 -0500

    companies posting to firebase now (#8)

--- src/routes/companies/companies.router.js
+++ src/routes/companies/companies.router.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
-const { getCompaniesController, getCompanyController } = require("./companies.controller");
+
+const { getCompaniesController, getCompanyController, addCompanyController } = require("./companies.controller");
 
 const router = express.Router();
 
 router.get("", getCompaniesController);
 router.get('/:id', getCompanyController);
-
+router.post('', addCompanyController);
+// console.log(router);
 module.exports = {
-  companiesRouter: router
+	companiesRouter: router
 };
